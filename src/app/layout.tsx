8be1a2b24c252b4de8500fb474c4f9e65b56b3ff
buhama <hassan@hassanahmed.ca>
2024-09-29 08:26:05 -0400

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html suppressHydrationWarning lang="en">
       <body className={inter.className}><ThemeProvider attribute="class">{children}</ThemeProvider></body>
+      <Analytics />
     </html>
   );
 }
